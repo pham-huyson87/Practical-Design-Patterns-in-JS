bbe7d6668fe9cb78400defbca3ea3203d03dac12
pham-huyson87 <pham.huyson87@hotmail.com>
2018-05-22 20:47:14 -0400

    Initial commit

---
+++ DesignPatternsInJS/Creational Design Patterns/Constructor Pattern/Template/ConstructorPattern.js
@@ -0,0 +1,20 @@
+﻿
+function Component(propertyA) {
+    
+    this.PropertyA = propertyA;
+    this.PropertyB = false;
+}
+
+Component.prototype.OperationA = function () {
+    this.PropertyB = true;
+};
+
+Component.prototype.OperationB = function () {
+    this.PropertyB = false;
+};
+
+
+var component = new Component("componentName");
+
+component.OperationA();
+component.OperationB(); 
---
+++ DesignPatternsInJS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DesignPatternsInJS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesignPatternsInJS")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48f0e0fa-2de3-4932-acd4-ade035fcdf6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
