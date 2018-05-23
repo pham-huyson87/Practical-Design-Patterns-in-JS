e48e70513644dfbe6bff6fbd22571c5291a52a3d
pham-huyson87 <pham.huyson87@hotmail.com>
2018-05-22 21:00:52 -0400

    Module Pattern
    Module Pattern

---
+++ DesignPatternsInJS/Creational Design Patterns/Module Pattern/Template/ModulePattern.js
@@ -0,0 +1,18 @@
+﻿
+var module = function () {
+    
+    var property = {};
+
+    var operationA = function () { return true; }
+    var operationB = function (number) { return number * 3; }
+
+    return {
+        operationA: operationA,
+        operationB: operationB
+    }
+}
+
+var moduleA = module();
+
+moduleA.operationA();
+moduleA.operationB(12);
