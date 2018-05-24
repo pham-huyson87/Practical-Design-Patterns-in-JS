08cffa2fa51ea14663484712bef3c9520ff940ac
pham-huyson87 <pham.huyson87@hotmail.com>
2018-05-24 18:31:02 -0400

    Factory Pattern
    Factory Pattern

---
+++ DesignPatternsInJS/Creational Design Patterns/Factory Pattern/Template/FactoryPattern.js
@@ -0,0 +1,24 @@
+﻿var objectFactory = function () {
+    
+    this.getObject = function (objectType) {
+        
+        if (objectType === "A") {
+
+            if (this.aObject) {
+                return this.aObject;
+            } else {
+                this.aObject = aObject();
+            }
+        }
+
+        if (objectType === "B") {
+
+            var bObject = bObject();
+            return bObject;
+        }
+    }
+};
+
+var factory = new objectFactory(),
+    typeAFactory = factory.getObject("A"),
+    typeBFactory = factory.getObject("B");
