be5843a5f690febbbef82cd0d9e48a81676c07aa
pham-huyson87 <pham.huyson87@hotmail.com>
2018-05-25 18:48:54 -0400

    Singleton Pattern
    Singleton Pattern

---
+++ DesignPatternsInJS/Creational Design Patterns/Singleton Pattern/Template/SingletonPattern.js
@@ -0,0 +1,22 @@
+﻿
+var Component = (function () {
+
+    var component;
+
+    function createComponent() {
+        var component = new Object();
+        return component
+    }
+
+    return {
+        getInstance: function () {
+            if (!component) {
+                component = createComponent();
+            }
+            return component;
+        }
+    };
+})();
+
+
+var component1 = Component.getInstance();
