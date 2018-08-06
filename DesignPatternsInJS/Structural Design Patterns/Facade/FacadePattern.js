5414195ae33b4362b04858f32e4bfb9685a8616e
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-06 17:45:45 -0400

    Facade Pattern
    Facade Pattern

---
+++ DesignPatternsInJS/Structural Design Patterns/Facade/FacadePattern.js
@@ -0,0 +1,42 @@
+﻿
+var LegacyService = function () {
+
+    return {
+        complete: function (task) { },
+        setCompleteDate: function (task) { },
+        notifyCompletion: function (task, user) { },
+        save: function (task) { }
+    };
+}();
+
+var myTask = new Task({
+    name: 'MyTask',
+    priority: 1,
+    project: 'Course',
+    user: 'Jon',
+    completed: false
+});
+
+
+// The Facade
+var TaskServiceWrapper = function () {
+
+    var completeAndNotify = function (task) {
+
+        LegacyService.complete(task);
+
+        if (task.complete == true) {
+            LegacyService.setCompleteDate(task)
+            LegacyService.notifyCompletion(task, task.user);
+            LegacyService.save(task);
+        }
+    }
+
+    return {
+        completeAndNotify: completeAndNotify
+    };
+}();
+
+
+// The use of the Facade
+TaskServiceWrapper.completeAndNotify(task);
