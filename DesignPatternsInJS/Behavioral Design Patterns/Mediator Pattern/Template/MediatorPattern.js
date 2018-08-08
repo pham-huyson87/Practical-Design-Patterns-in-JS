86647676a5c6564437358516988fd89ea72895c6
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-08 11:57:53 -0400

    Mediator Pattern
    Mediator Pattern

---
+++ DesignPatternsInJS/Behavioral Design Patterns/Mediator Pattern/Template/MediatorPattern.js
@@ -0,0 +1,63 @@
+﻿
+
+var mediator = (function () {
+    
+    var channels = {};
+
+    var subscribe = function (channel, context, func) {
+        
+        if (!this.channels[channel]) {
+            this.channels[channel] = [];
+        }
+
+        this.channels[channel].push({
+            context: context,
+            func: func
+        });
+    }
+    
+    var publish = function (channel) {
+
+        if (!this.channels[channel]) {
+            return false;
+        }
+
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        for (var i = 0; i < this.channels[channel][i]; i++) {
+            var subscriber = this.channels[channel][i];
+            subscriber.func.apply(this, args)
+        }
+    }
+
+    return {
+        channels: {},
+        subscribe: subscribe,
+        publish: publish
+    };
+
+}());
+
+
+
+var SubcriberComponent = function () {
+    this.operationOnPublish = function () { };
+};
+
+var Component = function () {
+    this.operation = function () { };
+};
+
+
+
+var component = new Component();
+var subcriberComponent = new SubcriberComponent();
+
+mediator.subscribe('communicationChannel', subcriberComponent, subcriberComponent.operationOnPublish);
+
+component.operation = function () {
+    mediator.publish('communicationChannel', this);
+    component.prototype.operation.call(this);
+}
+component.operation();
+
