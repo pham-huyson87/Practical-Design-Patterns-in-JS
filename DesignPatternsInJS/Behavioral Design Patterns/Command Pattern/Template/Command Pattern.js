03be652a68cd37a989faa9d67409823225f624ec
pham-huyson87 <pham.huyson87@hotmail.com>
2018-08-11 17:47:15 -0400

    Command Pattern
    Command Pattern

---
+++ DesignPatternsInJS/Behavioral Design Patterns/Command Pattern/Template/Command Pattern.js
@@ -0,0 +1,60 @@
+﻿
+var component = {
+
+    models: {},
+    commands: [],
+    
+    commandA: function (id) { },
+    commandB: function (model) {
+        component.models[model.id] = model;
+    },
+
+    replay: function () {
+        for (var i = 0; i < component.commands.length; i++) {
+            var command = component.commands[i];
+
+            component.executeNoLog(command.name, command.obj);
+        }
+    }
+
+};
+
+
+component.execute = function (commandName) {
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    component.commands.push({
+        name: commandName,
+        obj: args[0]
+    });
+
+    if (component[commandName]) {
+        return component[commandName].apply(component, args);
+    }
+    return false;
+};
+
+component.executeNoLog = function (commandName) {
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if (component[commandName]) {
+        return component[commandName].apply(component, args);
+    }
+    return false;
+};
+
+
+
+component.execute('commandA', 1);
+component.execute('commandB', { id: 2 });
+component.execute('commandA', 2);
+component.execute('commandB', { id: 3 });
+component.execute('commandA', 3);
+
+component.models = {};
+
+component.replay();
+
+
